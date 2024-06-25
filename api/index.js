2972490b60ef5a0d28fe2c917ff8827bed88733f
AkankshaGangil <akankshagangil2003@gmail.com>
2024-06-25 17:23:23 +0530

    create signup api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv' 
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI)
@@ -11,9 +13,24 @@ mongoose.connect(process.env.MONGODB_URI)
 ) .catch((err) => {
     console.log(err);
 })
-const app = express()
+const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
 
     console.log('server is running on port 3000')
 })
+
+app.use('/api/user',userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use((err,req,res,next) =>{
+    const statusCode =err.statusCode || 500;
+    const message =err.message || 'internal server error';
+    res.status(statusCode ).json({
+        success:false,
+        statusCode,
+        message,
+    });
+});
+
---
+++ api/utils/error.js
@@ -0,0 +1,8 @@
+
+
+export const errorHandler = (statusCode,messsage)=>{
+    const error = new Error();
+    error.statusCode = statusCode;
+    error.message=messsage;
+    return error;
+}
